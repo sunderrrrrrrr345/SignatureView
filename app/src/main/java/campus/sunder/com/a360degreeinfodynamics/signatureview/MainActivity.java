616a57ab592025d89e68136a9d4356fecfa0b207
Admin <sunder@pingtank.com>
2017-09-28 17:04:33 +0530

    Initial commit

---
+++ app/src/main/java/campus/sunder/com/a360degreeinfodynamics/signatureview/MainActivity.java
@@ -0,0 +1,33 @@
+package campus.sunder.com.a360degreeinfodynamics.signatureview;
+
+import android.annotation.TargetApi;
+import android.os.Build;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(new SignatureMainLayout(this));
+        //Permission for 23+ api
+        if (shouldAskPermissions()) {
+            askPermissions();
+        }
+    }
+    protected boolean shouldAskPermissions() {
+        return (Build.VERSION.SDK_INT > Build.VERSION_CODES.LOLLIPOP_MR1);
+    }
+
+    @TargetApi(23)
+    protected void askPermissions() {
+        String[] permissions = {
+                "android.permission.READ_EXTERNAL_STORAGE",
+                "android.permission.WRITE_EXTERNAL_STORAGE"
+        };
+        int requestCode = 200;
+        requestPermissions(permissions, requestCode);
+    }
+
+}
